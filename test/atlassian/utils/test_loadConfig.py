ca323c875ad25061f98712a013d41d326b72e1a7
Jörg Brüwer <joerg.bruewer@dbschenker.com>
2024-11-06 18:10:06 +0100

    Fixed blocked merge

---
+++ test/atlassian/utils/test_loadConfig.py
@@ -0,0 +1,18 @@
+import pytest
+import pandas as pd
+
+from atlassian.utils.loadConfig import loadConfig as conf
+
+
+class TestloadConfig():
+    
+    def test_getIssueProjects(self):
+
+        jiraProjects = conf.readConfig(self)
+        #check = jiraProjects[jiraProjects['jira_id']=='ODM']
+        #print(check['jira_id'])
+        assert jiraProjects.empty is False
+        #assert ( jiraProjects[(jiraProjects["jira_id"] == "ODM")].size == 1)
+
+tr = TestloadConfig()
+tr.test_getIssueProjects()
